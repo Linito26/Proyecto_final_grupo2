82738e04d63a7c12540588bc41cc2426fb9747fd
IVAN ONOFRE ZULETA <IVAN ONOFRE ZULETA@IVAN-ONOFRE26>
2022-10-10 20:21:29 -0600

    PRUEBA2

---
+++ src/prueba1/prueba2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba1;
+
+/**
+ *
+ * @author IVAN ONOFRE ZULETA
+ */
+public class prueba2 {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("ANALY AMARGADA 2");
+    }
+    
+    
+}
