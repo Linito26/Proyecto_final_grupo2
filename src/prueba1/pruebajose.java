ae3c527e035b35d3dad9d12893e5a88f0ae6f113
joseprogramacion2 <joseprogramacion2@gmail.com>
2022-10-11 19:55:06 -0600

    pruebass

--- src/prueba1/pruebajose.java
+++ src/prueba1/pruebajose.java
@@ -9,5 +9,8 @@ package prueba1;
  * @author Dell G3
  */
 public class pruebajose {
-    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("ANALY AMARGADA 3");
+    }
 }
