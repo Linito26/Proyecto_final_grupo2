d6abb4e8a4686b96ed9533482ac28fa35c45b769
joseprogramacion2 <joseprogramacion2@gmail.com>
2022-10-11 19:53:52 -0600

    pruebass

---
+++ src/prueba1/pruebajose.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba1;
+
+/**
+ *
+ * @author Dell G3
+ */
+public class pruebajose {
+    
+}
